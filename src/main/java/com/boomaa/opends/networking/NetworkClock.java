99ef84db2e92007e5a39f307d4fbbba9d43b6fe9
Marc DiFilippo <marc@marcdif.com>
2024-05-03 16:42:07 -0400

    Add debug logging

--- src/main/java/com/boomaa/opends/networking/NetworkClock.java
+++ src/main/java/com/boomaa/opends/networking/NetworkClock.java
@@ -34,7 +34,7 @@ public class NetworkClock extends Clock {
             if (connFms) {
                 if (DisplayEndpoint.NET_IF_INIT.get(remote, protocol)) {
                     if (!iface.write(DisplayEndpoint.CREATOR.create(remote, protocol))) {
-                        Debug.println(makeDebugStr("network error"), EventSeverity.WARNING, true);
+                        Debug.println(makeDebugStr("network error 1"), EventSeverity.WARNING, true);
                         DisplayEndpoint.UPDATER.update(ParserNull.getInstance(), remote, protocol);
                         reloadInterface();
                     } else {
@@ -47,12 +47,12 @@ public class NetworkClock extends Clock {
                             PacketParser packetParser = DisplayEndpoint.getPacketParser(remote, protocol, data);
                             DisplayEndpoint.UPDATER.update(packetParser, remote, protocol);
                             Debug.println(remote + " " + protocol + " interface connected to " + iface.toString(), EventSeverity.INFO, true);
-                            Debug.removeSticky(makeDebugStr("network error"));
+                            Debug.removeSticky(makeDebugStr("network error 2"));
                             Debug.removeSticky(makeDebugStr("invalid data"));
                         }
                     }
                 } else {
-                    Debug.println(makeDebugStr("network error"), EventSeverity.WARNING, true);
+                    Debug.println(makeDebugStr("network error 3"), EventSeverity.WARNING, true);
                     DisplayEndpoint.UPDATER.update(ParserNull.getInstance(), remote, protocol);
                     reloadInterface();
                 }
@@ -69,37 +69,50 @@ public class NetworkClock extends Clock {
     }
 
     public void reloadInterface() {
+        if (protocol.equals(Protocol.UDP)) System.out.println("A");
         PacketCounters.get(remote, protocol).reset();
         if (DisplayEndpoint.UPDATER != null) {
+            if (protocol.equals(Protocol.UDP)) System.out.println("B");
             DisplayEndpoint.UPDATER.update(ParserNull.getInstance(), remote, protocol);
         }
         if (iface != null) {
+            if (protocol.equals(Protocol.UDP)) System.out.println("C");
             Debug.removeSticky(remote + " " + protocol + " interface connected to " + iface);
             iface.close();
             iface = null;
         }
         boolean isFms = remote == Remote.FMS;
         if (isFms && !MainJDEC.FMS_CONNECT.isSelected()) {
+            if (protocol.equals(Protocol.UDP)) System.out.println("D");
             DisplayEndpoint.NET_IF_INIT.set(false, remote, protocol);
             return;
         }
+        if (protocol.equals(Protocol.UDP)) System.out.println("E");
         try {
+            if (protocol.equals(Protocol.UDP)) System.out.println("F");
             String ip = isFms
                     ? AddressConstants.FMS_IP
                     : AddressConstants.getRioAddress();
+            if (protocol.equals(Protocol.UDP)) System.out.println("G " + ip + " " + isFms);
             boolean reachable = exceptionPingTest(ip);
+            if (protocol.equals(Protocol.UDP)) System.out.println("H " + reachable);
             if (!reachable) {
+                if (protocol.equals(Protocol.UDP)) System.out.println("I");
                 uninitialize(isFms);
                 return;
             }
             PortTriple ports = isFms
                     ? AddressConstants.getFMSPorts()
                     : AddressConstants.getRioPorts();
+            if (protocol.equals(Protocol.UDP)) System.out.println("J " + ports);
             iface = protocol == Protocol.TCP
                     ? new TCPInterface(ip, ports.getTcp())
                     : new UDPInterface(ip, ports.getUdpClient(), ports.getUdpServer());
+            if (protocol.equals(Protocol.UDP)) System.out.println("K " + iface.toString());
             DisplayEndpoint.NET_IF_INIT.set(true, remote, protocol);
+            if (protocol.equals(Protocol.UDP)) System.out.println("L");
         } catch (IOException e) {
+            if (protocol.equals(Protocol.UDP)) System.out.println("M " + e.getMessage());
             uninitialize(isFms);
         }
     }
